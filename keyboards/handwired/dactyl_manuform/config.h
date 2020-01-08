8af375a3630df0290ce70b9a7658cc0c72c6e54c
Quentin <qlebastard@gmail.com>
2020-01-08 14:37:32 +0100

    change to 6x6,  make the thumb cluser mini

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -31,13 +31,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 // Rows are doubled-up
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 5
+#define MATRIX_ROWS 14
+#define MATRIX_COLS 6
 
-// wiring of each half
-#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
+#define DIODE_DIRECTION ROW2COL
+
+#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7, E6, B4 }
 // #define MATRIX_COL_PINS { B5, B4, E6, D7, C6 }
-#define MATRIX_COL_PINS { C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -51,13 +51,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = LAYOUT( \
-  KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                       KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    \
-  KC_A, KC_S, KC_D,    KC_F,    KC_G,                                       KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, \
-  KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, \
-              KC_LBRC, KC_RBRC,                                                   KC_MINS, KC_EQL,                   \
-                                      SFT_ESC, CTL_BSPC,  ALT_SPC, SFT_ENT,                                          \
-                                      KC_TAB,  KC_HOME,   KC_END,  KC_DEL,                                           \
-                                      RAISE,   KC_GRV,    KC_LGUI, LOWER
+// +1
+  KC_F1,   KC_F2,KC_F3,KC_F4,   KC_F5,   KC_F6,                                      KC_F7,KC_F8,   KC_F9,   KC_F10, KC_F10,  KC_F11,                              
+  KC_EQL,  KC_1, KC_2, KC_3,    KC_4,    KC_5,                                       KC_6, KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,\
+  KC_DELT, KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                       KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,   \
+  KC_BSPC, KC_A, KC_S, KC_D,    KC_F,    KC_G,                                       KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, GUI_T(KC_QUOT), \
+  KC_LSFT, KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, KC_RSFT, \ // TODO
+              KC_LBRC, KC_RBRC,                                                           KC_MINS, KC_EQL,                   \
+                                                 ALT_SPC,              SFT_ENT,                                          \
+                                               KC_TAB,  KC_HOME,   KC_END,  KC_DEL,                                           \
+                                               RAISE,   KC_GRV,    KC_LGUI, LOWER
 ),
 
 /* Raise
@@ -80,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                    |      |      | |      |      |
  *                                    '------+------' '------+------'
  */
-
+/*
 [_RAISE] = LAYOUT( \
   ____, ____,  KC_MU, ____,  ____,                          KC_VOLU, ____,    KC_UP,   ____,     KC_PGUP,   \
   ____, KC_ML, KC_MD, KC_MR, ____,                          KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDOWN, \
@@ -110,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                    |      |      | |      |      |
  *                                    '------+------' '------+------'
  */
-
+/*
 [_LOWER] = LAYOUT( \
   KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                            KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  \
   KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                             KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    \
@@ -119,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                              ____, ____,  ____, ____,                                               \
                                              ____, ____,  ____, ____,                                               \
                                              ____, ____,  ____, ____                                                \
-)
+)*/
 };
 
 void persistent_default_layer_set(uint16_t default_layer) {
