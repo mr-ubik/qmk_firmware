cbce90f6752d8908e0dc2fce3f21954e1da79b20
Quentin <qlebastard@gmail.com>
2020-01-08 14:41:45 +0100

    fix stray

--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -51,12 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = LAYOUT( \
-// +1
   KC_F1,   KC_F2,KC_F3,KC_F4,   KC_F5,   KC_F6,                                      KC_F7,KC_F8,   KC_F9,   KC_F10, KC_F10,  KC_F11,                              
   KC_EQL,  KC_1, KC_2, KC_3,    KC_4,    KC_5,                                       KC_6, KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,\
   KC_DELT, KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                       KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,   \
   KC_BSPC, KC_A, KC_S, KC_D,    KC_F,    KC_G,                                       KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, GUI_T(KC_QUOT), \
-  KC_LSFT, KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, KC_RSFT, \ // TODO
+  KC_LSFT, KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, KC_RSFT,  \
               KC_LBRC, KC_RBRC,                                                           KC_MINS, KC_EQL,                   \
                                                  ALT_SPC,              SFT_ENT,                                          \
                                                KC_TAB,  KC_HOME,   KC_END,  KC_DEL,                                           \
@@ -93,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    ____, ____,  ____, ____,                                                 \
                                    ____, ____,  ____, ____                                                  \
 ),
-/* Lower
+ Lower
  * ,----------------------------------,                             ,----------------------------------,
  * | F1   | F2   | F3   | F4   | F5   |                             |  F6  | F7   |  F8  |  F9  |  F10 |
  * |------+------+------+------+------|                             |-------------+------+------+------|
