084ad5ccd5358446a49658b1c5238337cf62c648
Quentin <qlebastard@gmail.com>
2020-01-13 10:44:51 +0100

    put back default pins

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -36,9 +36,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DIODE_DIRECTION ROW2COL
 
-#define MATRIX_ROW_PINS { D0, D4, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS { F5, F6, F7, B1, B3, B2, B6 }
 // #define MATRIX_COL_PINS { B5, B4, E6, D7, C6 }
-#define MATRIX_COL_PINS { B3, B1, F7, F6, F5, F4 }
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
 
 
 /* define if matrix has ghost */
